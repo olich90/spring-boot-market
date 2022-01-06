4aa1f6c76c2d2b9d3e7382e88a0990b51448a383
olich90 <olich_90@mail.ru>
2022-01-06 17:41:05 +0300

    hw12 заглушка

--- src/main/java/ru/gb/springbootdemoapp/SpringBootDemoAppApplication.java
+++ src/main/java/ru/gb/springbootdemoapp/SpringBootDemoAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition
+//hw12 заглушка
 public class SpringBootDemoAppApplication {
 
 	public static void main(String[] args) {
